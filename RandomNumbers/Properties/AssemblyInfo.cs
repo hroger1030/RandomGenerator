986ef827b4e62a2961d8000ce9c19959eb255079
roger.hill <hroger1030@gmail.com>
2017-05-10 23:51:54 -0700

    Nightly Update

--- RandomNumbers/Properties/AssemblyInfo.cs
+++ RandomNumbers/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RandomNumbers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Random Number Generator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GlobalConquestGames")]
 [assembly: AssemblyProduct("RandomNumbers")]
